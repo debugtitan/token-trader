4f77b4d1f0686f3811f6384ba13cd62c4137b1d0
debugtitan <alts.devs@gmail.com>
2024-08-29 22:44:02 +0100

    configuration file for app

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+# Load our environment variables
+load_dotenv(BASE_DIR,".env")
+
+RPC_NODE = os.getenv("RPC")
