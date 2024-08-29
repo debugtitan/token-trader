2dbd698ee2aa913c21ca849ace65b2b3a6e3fb14
debugtitan <alts.devs@gmail.com>
2024-08-30 00:14:02 +0100

    :sparkles: utils(config) keys file path file reader impl

--- utils/config.py
+++ utils/config.py
@@ -5,8 +5,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+KEYS_PATH = os.path.join(BASE_DIR, "keys.txt")
 # Load our environment variables
-load_dotenv(os.path.join(BASE_DIR,".env"))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 RPC_NODE = os.getenv("RPC")
+LAMPORTS_PER_SOL = 1_000_000_000
+
+def read_private_keys():
+    with open(KEYS_PATH, "r") as file:
+        return [line.strip() for line in file.readlines()]
