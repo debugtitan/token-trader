06af5280db4e4532d2b43820bc09ef534662a6ae
debugtitan <alts.devs@gmail.com>
2024-08-29 23:20:14 +0100

    utils(config) fix loading environment var

--- utils/config.py
+++ utils/config.py
@@ -7,6 +7,6 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Load our environment variables
-load_dotenv(BASE_DIR,".env")
+load_dotenv(os.path.join(BASE_DIR,".env"))
 
 RPC_NODE = os.getenv("RPC")
