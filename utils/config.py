f26968ceeb42e7f46471b22272a7de972d095d6e
debugtitan <alts.devs@gmail.com>
2024-08-30 00:47:43 +0100

    yadium authority and raydium pool

--- utils/config.py
+++ utils/config.py
@@ -11,6 +11,10 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 RPC_NODE = os.getenv("RPC")
 LAMPORTS_PER_SOL = 1_000_000_000
+RAYDIUM_LIQUIDITY_POOL = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8"
+RAYDIUM_AUTHORITY = "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1"
+TEST_TOKEN = "7T5xsZueWDi5o1o9mokVahiLfChn5aQaWRMbJHWYrvgX"
+
 
 def read_private_keys():
     with open(KEYS_PATH, "r") as file:
