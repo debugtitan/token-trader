ee0cc5891e45fc02c75bff8ae03a70cbe149427e
debugtitan <alts.devs@gmail.com>
2024-08-30 13:04:18 +0100

    trader(app)sell randomly 2-10%

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import asyncio
 import random
 import time
 from contextlib import suppress
-
-from solders.keypair import Keypair  # type: ignore
 from loguru import logger
 from utils.raydium import RaydiumClient
-from utils.config import TEST_TOKEN, TEST_AMM_KEY, read_private_keys, TOKEN_SUPPLY
+from utils.config import TEST_TOKEN, TEST_AMM_KEY, read_private_keys
 
 
-def check_supply_holdings(wallet_holding: float):
-    return wallet_holding >= 0.3 * TOKEN_SUPPLY
+def get_sell_amount(wallet_holding: float):
+    percentage_to_sell = random.uniform(2, 10)  # random 2-10%
+    sell_amount = (percentage_to_sell / 100) * wallet_holding
+    return sell_amount
 
 
 async def main():
@@ -21,25 +21,18 @@ async def main():
     wallet_sol_balance = await client.balance()
     wallet_address = await client.wallet_address()
     wallet_token_balance = await client.check_token_balance(TEST_TOKEN)
-    minimum_wallet_holdings = check_supply_holdings(wallet_token_balance)
+    sell_holdings = get_sell_amount(wallet_token_balance)
+    if sell_holdings <= 0:
+        logger.error("Invalid sell amount. Aborting swap.")
+        return
     logger.info(
-        f"""Wallet: {wallet_address}\nBal SOL: {wallet_sol_balance}\nToken Balance: {wallet_token_balance}"""
+        f"""Performing Sell \n\nWallet: {wallet_address}\nBal SOL: {wallet_sol_balance}\nToken Balance: {wallet_token_balance}\nAmount to sell: {sell_holdings}"""
     )
-    if minimum_wallet_holdings:
-        sell_percentage = random.uniform(0.02, 0.1)  # Randomly select 2-10%
-        amount_to_sell = wallet_token_balance * sell_percentage
-        await client.make_sell_swap(TEST_AMM_KEY)
-        logger.info(
-            f"Performing sell: {amount_to_sell} of {TEST_TOKEN} from wallet {wallet_address}."
-        )
-    else:
-        logger.info(
-            f"Wallet {wallet_address} skipped due to insufficient holdings.\nHoldings: {wallet_token_balance}"
-        )
+    await client.make_sell_swap(TEST_AMM_KEY, int(sell_holdings * 10**9))
 
 
 if __name__ == "__main__":
     with suppress(KeyboardInterrupt) as error:
         while True:
             asyncio.run(main())
-            time.sleep(60 * 2)
+            time.sleep(60)
