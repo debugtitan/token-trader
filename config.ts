1a614dbcc1a68f70655462b633ab2c7aaa5d6af7
debugtitan <alts.devs@gmail.com>
2024-09-15 12:24:22 +0100

    config settings

---
+++ config.ts
@@ -0,0 +1,16 @@
+import { PublicKey } from "@solana/web3.js"
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: ".env",
+});
+const Config = {
+    AMM_ID: "ATDyH3UarK8wEbjwKwzFgzvNsw7UCC2uaTWFaEHZAxLW",
+    WSOL: new PublicKey("So11111111111111111111111111111111111111112"),
+    MINT: new PublicKey("8Eewax7ooBdi5nwkp7VwittjEV9mVWAGhN1KVRJroeMR"),
+    sellPercentage: Number(process.env.WALLET_HOLDINGS_BEFORE_SELL),
+    randomTrade1: Number(process.env.RANDOM_TRADE_PERCENTAGE_START),
+    randomTrade2: Number(process.env.RANDOM_TRADE_PERCENTAGE_END)
+}
+
+export { Config }
